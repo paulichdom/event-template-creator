8e8b7d617dbb816e6f99e0545d004f243d5f1a7b
paulichdom <paulic.domagoj@gmail.com>
2025-07-09 05:42:56 +0200

    add emotion and mui support

--- src/sidepanel/main.tsx
+++ src/sidepanel/main.tsx
@@ -5,6 +5,11 @@ import App from "./App.tsx";
 import "@ant-design/v5-patch-for-react-19";
 import "./index.css";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
